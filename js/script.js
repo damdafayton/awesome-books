417be843437e44d6658b32c7cb34f8cfa012778f
backtofayton <yucelolcay@gmail.com>
2022-02-07 16:24:34 +0200

    Add remove handler

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-const books = [];
+let books = [];
 const addBtn = document.getElementById("addBtn");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 
 function addNewBook(title, author) {
-  books.push({ title, author });
+    books.push({ title, author });
 }
 
 function removeBook(title) {
-  books.filter((book) => book.title === title);
+    books = books.filter((book) => book.title === title);
 }
 
 function addBook(e) {
-  e.preventDefault();
-  addNewBook(title.value, author.value);
-  const ul = document.querySelector("#book-list");
-  const li = document.createElement("li");
+    e.preventDefault();
+    addNewBook(title.value, author.value);
+    const ul = document.querySelector("#book-list");
+    const li = document.createElement("li");
 
-  const titleElem = document.createElement("p");
-  titleElem.innerText = title.value;
-  const authorElem = document.createElement("p");
-  authorElem.innerText = author.value;
-  const button = document.createElement("button");
-  button.innerText = "Remove";
-  li.appendChild(title);
-  li.appendChild(author);
-  li.appendChild(button);
+    const titleElem = document.createElement("h2");
+    titleElem.innerText = title.value;
+    const authorElem = document.createElement("p");
+    authorElem.innerText = author.value;
+    const button = document.createElement("button");
+    button.innerText = "Remove";
+    button.addEventListener('click', (e) => {
+        console.log(e.target)
+        const liElem = e.target.parentElement
+        let title = liElem.querySelector('h2')
+        title = title.innerText
+        removeBook(title)
+        liElem.remove()
+    })
 
-  ul.appendChild(li);
+    li.appendChild(titleElem);
+    li.appendChild(authorElem);
+    li.appendChild(button);
+
+    ul.appendChild(li);
 }
 
 addBtn.addEventListener("click", addBook);

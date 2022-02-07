2f8f1d5cbde28a2539185cfc487b5b98742d75c6
ayubch <ayoub.chahir96@gmail.com>
2022-02-07 14:58:15 +0100

    Add Button interaction

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 const books = [];
+const addBtn = document.getElementById("addBtn");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
 function addNewBook(title, author) {
-    books.push({ title, author })
+  books.push({ title, author });
 }
 
 function removeBook(title) {
-    books.filter(book => book.title === title);
+  books.filter((book) => book.title === title);
 }
 
-books.forEach(book => {
-    const ul = document.querySelector('#book-list')
-    const li = document.createElement('li')
+function addBook(e) {
+  e.preventDefault();
+  addNewBook(title.value, author.value);
+  const ul = document.querySelector("#book-list");
+  const li = document.createElement("li");
 
-    const title = document.createElement('p')
-    title.innerText = book.title
-    const author = document.createElement('p')
-    author.innerText = book.author
-    const button = document.createElement('button')
-    button.innerText = 'Remove'
-    li.appendChild(title)
-    li.appendChild(author)
-    li.appendChild(button)
+  const titleElem = document.createElement("p");
+  titleElem.innerText = title.value;
+  const authorElem = document.createElement("p");
+  authorElem.innerText = author.value;
+  const button = document.createElement("button");
+  button.innerText = "Remove";
+  li.appendChild(title);
+  li.appendChild(author);
+  li.appendChild(button);
 
-    ul.appendChild(li)
-})
+  ul.appendChild(li);
+}
+
+addBtn.addEventListener("click", addBook);

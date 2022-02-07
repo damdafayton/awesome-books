c72802c42d278933e3954fce74854cb68ab3d2f6
backtofayton <yucelolcay@gmail.com>
2022-02-07 16:52:27 +0200

    Fix linter

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 let books = [];
-const addBtn = document.getElementById("addBtn");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
+const addBtn = document.getElementById('addBtn');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
 const store = window.localStorage;
 
 function addNewBook(title, author) {
   books.push({ title, author });
-  store.setItem("books", JSON.stringify(books));
+  store.setItem('books', JSON.stringify(books));
 }
 
 function removeBook(title) {
   books = books.filter((book) => book.title === title);
+  store.setItem('books', JSON.stringify(books));
 }
 
 function addBook(e) {
   e.preventDefault();
   addNewBook(title.value, author.value);
-  const ul = document.querySelector("#book-list");
-  const li = document.createElement("li");
+  const ul = document.querySelector('#book-list');
+  const li = document.createElement('li');
 
-  const titleElem = document.createElement("h2");
+  const titleElem = document.createElement('h2');
   titleElem.innerText = title.value;
-  const authorElem = document.createElement("p");
+  const authorElem = document.createElement('p');
   authorElem.innerText = author.value;
-  const button = document.createElement("button");
-  button.innerText = "Remove";
-  button.addEventListener("click", (e) => {
-    console.log(e.target);
+  const button = document.createElement('button');
+  button.innerText = 'Remove';
+  button.addEventListener('click', (e) => {
     const liElem = e.target.parentElement;
-    let title = liElem.querySelector("h2");
+    let title = liElem.querySelector('h2');
     title = title.innerText;
     removeBook(title);
     liElem.remove();
@@ -41,4 +41,4 @@ function addBook(e) {
   ul.appendChild(li);
 }
 
-addBtn.addEventListener("click", addBook);
+addBtn.addEventListener('click', addBook);

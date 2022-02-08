03a8f23e7d1e423dcf4e2accbde57eed5d8ce616
backtofayton <yucelolcay@gmail.com>
2022-02-08 16:10:45 +0200

    Add remove method to class

--- js/script.js
+++ js/script.js
@@ -4,50 +4,51 @@ const author = document.getElementById('author');
 const store = window.localStorage;
 
 class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
 
-  static books = [];
+    static books = [];
 
-  addNewBook() {
-    Book.books.push({ title: this.title, author: this.author });
-    store.setItem('books', JSON.stringify(Book.books));
-  }
+    addNewBook() {
+        Book.books.push({ title: this.title, author: this.author });
+        store.setItem('books', JSON.stringify(Book.books));
+    }
 
-  removeBook() {
-    Book.books = Book.books.filter((book) => book.title === this.title);
-    store.setItem('books', JSON.stringify(Book.books));
-  }
+    static removeBook(title) {
+        console.log('hi')
+        Book.books = Book.books.filter((book) => book.title === title);
+        store.setItem('books', JSON.stringify(Book.books));
+    }
 }
 
 function addBook(e) {
-  e.preventDefault();
-  const book = new Book(title.value, author.value);
-  book.addNewBook();
-  const ul = document.querySelector('#book-list');
-  const li = document.createElement('li');
+    e.preventDefault();
+    const book = new Book(title.value, author.value);
+    book.addNewBook();
+    const ul = document.querySelector('#book-list');
+    const li = document.createElement('li');
 
-  const titleElem = document.createElement('h2');
-  titleElem.innerText = title.value;
-  const authorElem = document.createElement('p');
-  authorElem.innerText = author.value;
-  const button = document.createElement('button');
-  button.innerText = 'Remove';
-  button.addEventListener('click', (e) => {
-    const liElem = e.target.parentElement;
-    let title = liElem.querySelector('h2');
-    title = title.innerText;
-    removeBook();
-    liElem.remove();
-  });
+    const titleElem = document.createElement('h2');
+    titleElem.innerText = title.value;
+    const authorElem = document.createElement('p');
+    authorElem.innerText = author.value;
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+    button.addEventListener('click', (e) => {
+        const liElem = e.target.parentElement;
+        let title = liElem.querySelector('h2');
+        title = title.innerText;
+        Book.removeBook(title);
+        liElem.remove();
+    });
 
-  li.appendChild(titleElem);
-  li.appendChild(authorElem);
-  li.appendChild(button);
+    li.appendChild(titleElem);
+    li.appendChild(authorElem);
+    li.appendChild(button);
 
-  ul.appendChild(li);
+    ul.appendChild(li);
 }
 
 addBtn.addEventListener('click', addBook);
